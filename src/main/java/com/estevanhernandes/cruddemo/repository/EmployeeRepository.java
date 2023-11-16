f6598af7474d742d83775fff11fbe152c7ba4342
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2023-11-16 07:42:15 -0300

    add spring boot data rest

--- src/main/java/com/estevanhernandes/cruddemo/repository/EmployeeRepository.java
+++ src/main/java/com/estevanhernandes/cruddemo/repository/EmployeeRepository.java
@@ -2,5 +2,7 @@ package com.estevanhernandes.cruddemo.repository;
 
 import com.estevanhernandes.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
